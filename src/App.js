0043617391f6524e162153fbbf9dc03a233b91ad
Ian Christiansen <ianc.songdog@gmail.com>
2022-01-03 16:47:29 -0800

    added file structure and started pathing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Dogs from './views/Dogs/Dogs.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavLink to="/">Adoptable Dogs</NavLink>
+        <Switch>
+          <Route path="/" component={Dogs} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
