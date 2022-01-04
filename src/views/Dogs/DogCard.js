905c88e54fa18913c5bc7f7997d2dda9e6e1575a
Ian Christiansen <ianc.songdog@gmail.com>
2022-01-04 14:18:21 -0800

    rendered complete dog cards

--- src/views/Dogs/DogCard.js
+++ src/views/Dogs/DogCard.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
 import { getDogs } from '../../services/dogs';
+import './Dogs.css';
 
 export default function DogCard() {
   const [dogs, setDogs] = useState([]);
@@ -11,15 +11,19 @@ export default function DogCard() {
       const data = await getDogs();
       setDogs(data);
       setLoading(false);
+      // console.log(data);
     };
     fetchData();
   }, []);
   if (loading) return <h1>loading...</h1>;
   return (
-    <div className="dog-card">
-      help
+    <div className="dog-container">
       {dogs.map((dog) => (
-        <p key={dog.id}>{dog.name}</p>
+        <div className="dog-card" key={dog.id}>
+          <p>{dog.name}</p>
+          <p>{dog.breed}</p>
+          <img src={dog.image} className="image-preview"></img>
+        </div>
       ))}
     </div>
   );
