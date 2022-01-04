5039e27fcd6d9f2e74664aed6d2067ebd06ea607
Ian Christiansen <ianc.songdog@gmail.com>
2022-01-04 14:55:31 -0800

    individual dog pages linked and rendering

--- src/services/dogs.js
+++ src/services/dogs.js
@@ -4,3 +4,8 @@ export async function getDogs() {
   const resp = await client.from('dogs').select('*').order('name');
   return checkError(resp);
 }
+
+export async function getDogById(id) {
+  const resp = await client.from('dogs').select('*').match({ id });
+  return checkError(resp);
+}
--- src/views/Dogs/Dog.js
+++ src/views/Dogs/Dog.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { getDogById } from '../../services/dogs';
+import './Dogs.css';
 
-export default function Dog() {
-  return <div>Dog Info</div>;
+export default function Dog(props) {
+  const [dog, setDog] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getDogById(props.match.params.id);
+      setDog(data[0]);
+      setLoading(false);
+    };
+    fetchData();
+  }, [props.match.params.id]);
+  if (loading) return <h1>loading</h1>;
+
+  return (
+    <div className="dog-info">
+      <h1>{dog.name}</h1>
+      <img src={dog.image} className="dog-image"></img>
+      <h2>{dog.breed}</h2>
+      <p>{dog.age} year(s) old.</p>
+      <p>{dog.bio}</p>
+    </div>
+  );
 }
