287b569d3cf1089a3539e9a8b1fa8564d2d6136e
Ian Christiansen <ianc.songdog@gmail.com>
2022-01-05 15:22:09 -0800

    created DogForm component

---
+++ src/components/Dog/DogForm.js
@@ -0,0 +1,93 @@
+import React from 'react';
+
+export default function DogForm({
+  name,
+  setName,
+  bio,
+  setBio,
+  image,
+  setImage,
+  age,
+  setAge,
+  breed,
+  setBreed,
+  species,
+  setSpecies,
+}) {
+  return (
+    <div className="update">
+      <form>
+        <h2>update pet</h2>
+        <div className="control">
+          <label>name:</label>
+          <input
+            placeholder="Name"
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <div className="control">
+          <label>species:</label>
+          <input
+            placeholder="species"
+            type="text"
+            value={species}
+            onChange={(e) => {
+              setSpecies(e.target.value);
+            }}
+          />
+        </div>
+        <div className="control">
+          <label>breed:</label>
+          <input
+            placeholder="Name"
+            type="text"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          />
+        </div>
+        <div className="control">
+          <label>age:</label>
+          <input
+            placeholder="age"
+            type="text"
+            value={age}
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+          />
+        </div>
+        <div className="control">
+          <label>image(url):</label>
+          <input
+            placeholder="image"
+            type="text"
+            value={image}
+            onChange={(e) => {
+              setImage(e.target.value);
+            }}
+          />
+        </div>
+        <div className="control">
+          <label>bio:</label>
+          <input
+            placeholder="description"
+            type="text"
+            value={bio}
+            onChange={(e) => {
+              setBio(e.target.value);
+            }}
+          />
+        </div>
+        <div className="button">
+          <button>Save</button>
+        </div>
+      </form>
+    </div>
+  );
+}
